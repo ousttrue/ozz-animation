7e04dce2184de845f53371e78c9919d2362bac9e
ousttrue <ousttrue@gmail.com>
2024-08-28 15:21:45 +0900

    fix setup_dir

--- build.zig
+++ build.zig
@@ -27,24 +27,24 @@ fn prefixFromMesonBuild(
     b: *std.Build,
     optimize: std.builtin.OptimizeMode,
 ) std.Build.LazyPath {
-    const builddir = if (optimize == .Debug) "build_native_debug" else "build_native_release";
+    const builddir = b.path(if (optimize == .Debug) "build_native_debug" else "build_native_release");
+    const setup_dir = builddir.getPath(b);
     const prefix = b.path(if (optimize == .Debug) "prefix_debug" else "prefix_release");
     const meson_install = b.addSystemCommand(&.{
         "meson",
         "install",
         "-C",
-        builddir,
+        setup_dir,
     });
     step.dependOn(&meson_install.step);
 
-    const setup_dir = b.path(builddir).getPath(b);
     if (std.fs.openDirAbsolute(setup_dir, .{})) |*dir| {
         @constCast(dir).close();
     } else |_| {
         const meson_setup = b.addSystemCommand(&.{
             "meson",
             "setup",
-            builddir,
+            setup_dir,
             "--buildtype",
             if (optimize == .Debug) "debug" else "release",
             "--prefix",
