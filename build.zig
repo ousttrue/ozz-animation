589ebd82b1cf8e6a89ce3613cb329dfe661c97ae
ousttrue <ousttrue@gmail.com>
2024-08-28 15:05:07 +0900

    zig build

---
+++ build.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    if (target.result.isWasm()) {
+        @panic("todo: wasm not implemented");
+    } else {
+        dllToWriteFile(b.default_step, b, optimize);
+    }
+}
+
+fn dllToWriteFile(
+    step: *std.Build.Step,
+    b: *std.Build,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    const wf = b.addNamedWriteFiles("meson_build");
+    step.dependOn(&wf.step);
+    const prefix = prefixFromMesonBuild(&wf.step, b, optimize);
+    _ = wf.addCopyFile(prefix.path(b, "bin/ozz-animation.dll"), "ozz-animation.dll");
+    _ = wf.addCopyFile(prefix.path(b, "lib/ozz-animation.lib"), "ozz-animation.lib");
+}
+
+fn prefixFromMesonBuild(
+    step: *std.Build.Step,
+    b: *std.Build,
+    optimize: std.builtin.OptimizeMode,
+) std.Build.LazyPath {
+    const builddir = if (optimize == .Debug) "build_native_debug" else "build_native_release";
+    const prefix = b.path(if (optimize == .Debug) "prefix_debug" else "prefix_release");
+    const meson_install = b.addSystemCommand(&.{
+        "meson",
+        "install",
+        "-C",
+        builddir,
+    });
+    step.dependOn(&meson_install.step);
+
+    const setup_dir = b.path(builddir).getPath(b);
+    if (std.fs.openDirAbsolute(setup_dir, .{})) |*dir| {
+        @constCast(dir).close();
+    } else |_| {
+        const meson_setup = b.addSystemCommand(&.{
+            "meson",
+            "setup",
+            builddir,
+            "--buildtype",
+            if (optimize == .Debug) "debug" else "release",
+            "--prefix",
+        });
+        meson_setup.addFileArg(prefix);
+        meson_install.step.dependOn(&meson_setup.step);
+    }
+
+    return prefix;
+}
