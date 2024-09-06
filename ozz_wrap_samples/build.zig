a35c42401fda8e7f848182f3ce795c8825564d2e
ousttrue <ousttrue@gmail.com>
2024-09-06 15:37:27 +0900

    sokol clear

--- ozz_wrap_samples/build.zig
+++ ozz_wrap_samples/build.zig
@@ -1,8 +1,13 @@
 const std = @import("std");
+const zcc = @import("../zcc.zig");
+
 const Sample = struct {
     name: []const u8,
-    srcs: []const []const u8 = &.{},
-    cflags: []const []const u8 = &.{},
+    c_files: []const []const u8 = &.{},
+    c_flags: []const []const u8 = &.{},
+    cpp_files: []const []const u8 = &.{},
+    cpp_flags: []const []const u8 = &.{},
+    libs: []const []const u8 = &.{},
     fn build(
         self: @This(),
         b: *std.Build,
@@ -15,14 +20,29 @@ const Sample = struct {
             .name = self.name,
         });
 
+        // c
+        exe.linkLibC();
         exe.addCSourceFiles(.{
-            .files = self.srcs,
+            .files = self.c_files,
+            .flags = self.c_flags,
         });
+        // cpp
+        exe.linkLibCpp();
+        exe.addCSourceFiles(.{
+            .files = self.cpp_files,
+            .flags = self.cpp_flags,
+        });
+        // libs
+        for (self.libs) |lib| {
+            exe.linkSystemLibrary(lib);
+        }
+
         const sokol_dep = b.dependency("sokol", .{});
         exe.addIncludePath(sokol_dep.path(""));
 
         const install = b.addInstallArtifact(exe, .{});
         b.getInstallStep().dependOn(&install.step);
+        install.step.dependOn(zcc.createStep(b, .{ .targets = &.{exe} }));
 
         const run = b.addRunArtifact(exe);
         run.step.dependOn(&install.step);
@@ -48,11 +68,20 @@ pub fn build(
 const samples = [_]Sample{
     .{
         .name = "ozz_wrap_playback",
-        .srcs = &.{
-            "ozz_wrap_samples/sample_playback.cc",
+        .c_files = &.{
+            "ozz_wrap_samples/playback/main.c",
         },
-        .cflags = &.{
+        .c_flags = &.{
             "-std=c99",
+        },
+        .cpp_files = &.{
+            "ozz_wrap_samples/playback/sample_playback.cc",
+        },
+        .cpp_flags = &.{
+            "-std=c++20",
+        },
+        .libs = &.{
+            "gdi32",
         },
     },
 };
